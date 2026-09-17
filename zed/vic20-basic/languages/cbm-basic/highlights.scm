4fba54ce609f06d34ecc82df15d43d7eb6359070
Steven Combs <steven.combs@gmail.com>
2026-09-17 15:14:34 -0400

    Ship a prebuilt grammar wasm so CBM BASIC actually registers.
    vic20-basic (already installed) now includes grammars/cbm_basic.wasm
    and path suffixes for .bas/.m65. Snippets use VS Code array bodies
    and snippets.json so Tab can expand wht even before the language
    attaches.

--- zed/vic20-basic/languages/cbm-basic/highlights.scm
+++ zed/vic20-basic/languages/cbm-basic/highlights.scm
@@ -1,5 +1,6 @@
-(string) @string
-(number) @number
-(true) @constant
-(false) @constant
-(null) @constant
+(comment) @comment
+(tag_name) @keyword
+(attribute_name) @attribute
+(attribute_value) @string
+(text) @string
+(doctype) @constant
